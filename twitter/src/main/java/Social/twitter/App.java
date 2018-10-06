5de0e54ba657816afd86e2cd167a599cfa45c6cc
Madurai <Madurai@DESKTOP-Q25I2FE>
2018-10-06 21:49:13 +0530

    Test commit

---
+++ twitter/src/main/java/Social/twitter/App.java
@@ -0,0 +1,13 @@
+package Social.twitter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ twitter/src/test/java/Social/twitter/testng.java
@@ -0,0 +1,19 @@
+package Social.twitter;
+
+import org.testng.annotations.Test;
+
+public class testng {
+	
+	@Test
+	
+	public void LoginPage()
+	
+	{
+	
+		System.out.println("Maven test");
+		
+	}
+	
+
+}
+
